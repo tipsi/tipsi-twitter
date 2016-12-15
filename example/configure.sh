191752ab65bc5e12cb8e9edfac01120c86c5ad73
Denis <denis.sedura@gmail.com>
2016-12-15 15:34:57 +0200

    Update configure.sh

--- example/configure.sh
+++ example/configure.sh
@@ -5,3 +5,5 @@ sed -i.bak 's@<TWITTER_SECRET>@'"$TWITTER_SECRET"'@' android/app/src/main/java/c
 rm -rf android/app/src/main/java/com/example/MainApplication.java.bak
 
 /usr/libexec/PlistBuddy -x -c "Set :Fabric:APIKey $FABRIC_APIKEY" ./ios/example/Info.plist
+infoplist_api_key=`/usr/libexec/PlistBuddy -c "Print :Fabric:APIKey" ./ios/example/Info.plist`
+echo "Fabric API Key set to: $infoplist_api_key"
