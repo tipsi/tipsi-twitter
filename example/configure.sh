6fc958db2709bd92d912810a4c11d2a52c849623
Anton Kuznetsov <isnifer@gmail.com>
2016-12-16 16:25:41 +0300

    Update configure.sh for target os type

--- example/configure.sh
+++ example/configure.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-sed -i.bak 's@<TWITTER_KEY>@'"$TWITTER_KEY"'@' index.ios.js
-sed -i.bak 's@<TWITTER_SECRET>@'"$TWITTER_SECRET"'@' index.ios.js
-rm -rf index.ios.js.bak
+case "${TRAVIS_OS_NAME}" in
+  osx)
+    sed -i.bak 's@<TWITTER_KEY>@'"$TWITTER_KEY"'@' index.ios.js
+    sed -i.bak 's@<TWITTER_SECRET>@'"$TWITTER_SECRET"'@' index.ios.js
+    rm -rf index.ios.js.bak
 
-sed -i.bak 's@<TWITTER_KEY>@'"$TWITTER_KEY"'@' index.android.js
-sed -i.bak 's@<TWITTER_SECRET>@'"$TWITTER_SECRET"'@' index.android.js
-rm -rf index.android.js.bak
-
-/usr/libexec/PlistBuddy -x -c "Set :Fabric:APIKey $FABRIC_APIKEY" ./ios/example/Info.plist
+    /usr/libexec/PlistBuddy -x -c "Set :Fabric:APIKey $FABRIC_APIKEY" ./ios/example/Info.plist
+  ;;
+  linux)
+    sed -i.bak 's@<TWITTER_KEY>@'"$TWITTER_KEY"'@' index.android.js
+    sed -i.bak 's@<TWITTER_SECRET>@'"$TWITTER_SECRET"'@' index.android.js
+    rm -rf index.android.js.bak
+  ;;
+esac
