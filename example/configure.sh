b26cf77a8d198e5ed7a2e707927b25c245ea8bad
dmitriy <dmitriy.malec@gmail.com>
2016-12-19 15:15:28 +0200

    - fixes in configure

--- example/configure.sh
+++ example/configure.sh
@@ -4,3 +4,6 @@ sed -i.bak 's@<TWITTER_KEY>@'"$TWITTER_KEY"'@' index.android.js
 sed -i.bak 's@<TWITTER_SECRET>@'"$TWITTER_SECRET"'@' index.android.js
 rm -rf index.android.js.bak
 
+sed -i.bak 's@<FABRIC_APIKEY>@'"$FABRIC_APIKEY"'@' android/app/src/main/AndroidManifest.xml
+rm -rf android/app/src/main/AndroidManifest.xml.bak
+
