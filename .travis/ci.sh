563b3353588e505fa48a4ed14362eab01b3b3f7e
Dmytro Zavgorodniy <dmytro.zavgorodniy@gmail.com>
2016-12-23 13:28:47 +0200

    Update .travis/ci.sh (npm run configure:ios/npm run configure:android)

--- .travis/ci.sh
+++ .travis/ci.sh
@@ -3,13 +3,13 @@
 case "${TRAVIS_OS_NAME}" in
   osx)
     cd example_tmp
-    npm run configure
+    npm run configure:ios
     set -o pipefail && npm run build:ios | xcpretty -c -f `xcpretty-travis-formatter`
     npm run test:ios
   ;;
   linux)
     cd example_tmp
-    npm run configure
+    npm run configure:android
     npm run build:android
     npm run test:android
   ;;
