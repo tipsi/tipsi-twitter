1281542d94184d53e819b85c25223eef5a073bcb
Dmytro Zavgorodniy <dmytro.zavgorodniy@gmail.com>
2016-12-27 11:40:52 +0200

    General example app (Root.js)

--- example/index.ios.js
+++ example/index.ios.js
@@ -1,44 +1,4 @@
-import React, { Component } from 'react'
-import {
-  Alert,
-  AppRegistry,
-  StyleSheet,
-  Button,
-  View,
-} from 'react-native'
-import { TPSTwitterModule } from 'tipsi-twitter'
+import { AppRegistry } from 'react-native'
+import Root from './src/Root'
 
-TPSTwitterModule.init({
-  consumerKey: '<TWITTER_KEY>',
-  consumerSecret: '<TWITTER_SECRET>',
-})
-
-export default class example extends Component {
-  handleTwitterPress = async () => {
-    const result = await TPSTwitterModule.login()
-    Alert.alert(
-      'Login success',
-      `@${result.userName}`,
-      [{ text: 'OK' }]
-    )
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Button title="Login via Twitter" onPress={this.handleTwitterPress} />
-      </View>
-    )
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-})
-
-AppRegistry.registerComponent('example', () => example)
+AppRegistry.registerComponent('example', () => Root)
---
+++ example/src/Root.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+} from 'react-native'
+import { TPSTwitterModule } from 'tipsi-twitter'
+
+TPSTwitterModule.init({
+  twitter_key: '<TWITTER_KEY>',
+  twitter_secret: '<TWITTER_SECRET>',
+})
+
+export default class Root extends Component {
+  state = {
+    twitterUserName: '',
+    errorMessage: '',
+  }
+
+  handleCustomLoginPress = async () => {
+    try {
+      const result = await TPSTwitterModule.login()
+      this.setState({
+        errorMessage: '',
+        twitterUserName: result.userName,
+      })
+    } catch (error) {
+      this.setState({
+        errorMessage: error.message,
+        twitterUserName: '',
+      })
+    }
+  }
+
+  render() {
+    const { twitterUserName, errorMessage } = this.state
+
+    return (
+      <View style={styles.container}>
+        <Button
+          title="Login Button"
+          accessible
+          accessibilityLabel="loginButton"
+          onPress={this.handleCustomLoginPress}
+        />
+        <Text
+          accessibilityLabel="twitter_response"
+          style={styles.instructions}>
+          { twitterUserName !== '' ? `twitterUserName: ${twitterUserName}` : ''}
+        </Text>
+        <Text
+          accessibilityLabel="error_message"
+          style={styles.error}>
+          {errorMessage}
+        </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  error: {
+    fontSize: 20,
+    textAlign: 'center',
+    color: '#FF0000',
+  },
+  button: {
+    marginBottom: 20,
+    padding: 20,
+  },
+})
