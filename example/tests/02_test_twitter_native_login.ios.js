c4bc14460a48b8d9df1ec2276992641e524aebec
Dmytro Zavgorodniy <dmytro.zavgorodniy@gmail.com>
2016-12-27 11:41:49 +0200

    Unify tests

---
+++ example/tests/02_test_twitter_native_login.ios.js
@@ -0,0 +1,55 @@
+import test from 'tape-async'
+import helper from './utils/helper'
+
+const {
+  TWITTER_USER,
+  TWITTER_PASS,
+} = process.env
+
+const { driver, idFromXPath, idFromAccessId } = helper
+
+test('Test Twitter Native Login', async (t) => {
+  const loginButtonID = idFromAccessId('loginButton')
+
+  const usersActionSheetID = idFromXPath(`
+    //XCUIElementTypeApplication/XCUIElementTypeWindow/XCUIElementTypeOther[2]/
+    XCUIElementTypeSheet
+  `)
+
+  const firstUserButtonID = idFromXPath(`
+    /XCUIElementTypeApplication/XCUIElementTypeWindow/XCUIElementTypeOther[2]/
+    XCUIElementTypeSheet/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther[2]/
+    XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther
+  `)
+
+  const userNameTextID = idFromXPath(`
+    //XCUIElementTypeApplication/XCUIElementTypeWindow/XCUIElementTypeOther/XCUIElementTypeOther/
+    XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeStaticText
+  `)
+
+  try {
+    await driver.waitForVisible(loginButtonID, 30000)
+    t.pass('The user should be able to see "Login Button" button')
+
+    await driver.click(loginButtonID)
+    t.pass('The user should be able to click on "Login Button" button')
+
+    await driver.waitForVisible(usersActionSheetID, 30000)
+    t.pass('The user should be able to see list of twitter accounts')
+
+    await driver.click(firstUserButtonID)
+    t.pass('The user should be able to choose first twitter account')
+
+    await driver.waitForVisible(userNameTextID, 30000)
+    t.pass('The user should be able to see user name label')
+
+    const userNameText = `twitterUserName: ${TWITTER_USER}`
+    const sut_userNameText = await driver.getText(userNameTextID);
+    t.equal(sut_userNameText, userNameText, `The user should be able to see user name label with text: "${userNameText}"`);
+  } catch (error) {
+    await helper.screenshot()
+    await helper.source()
+
+    throw error
+  }
+})
