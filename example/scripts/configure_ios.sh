275d38f99740230299f7ef3d1636a65896be60e0
Dmytro Zavgorodniy <dmytro.zavgorodniy@gmail.com>
2016-12-27 12:53:56 +0200

    Update configure_ios script

--- example/scripts/configure_ios.sh
+++ example/scripts/configure_ios.sh
@@ -4,11 +4,4 @@ sed -i.bak 's@<TWITTER_KEY>@'"$TWITTER_KEY"'@' ./src/Root.js
 sed -i.bak 's@<TWITTER_SECRET>@'"$TWITTER_SECRET"'@' ./src/Root.js
 rm -rf ./src/Root.js.bak
 
-/usr/libexec/PlistBuddy -c "Add :Fabric dict" ./ios/example/Info.plist
-/usr/libexec/PlistBuddy -c "Add :Fabric:APIKey string" ./ios/example/Info.plist
-/usr/libexec/PlistBuddy -c "Add :Fabric:Kits array" ./ios/example/Info.plist
-/usr/libexec/PlistBuddy -c "Add :Fabric:Kits: dict" ./ios/example/Info.plist
-/usr/libexec/PlistBuddy -c "Add :Fabric:Kits:0:KitName string Twitter" ./ios/example/Info.plist
-/usr/libexec/PlistBuddy -c "Add :Fabric:Kits:0:KitInfo dict" ./ios/example/Info.plist
-
 /usr/libexec/PlistBuddy -x -c "Set :Fabric:APIKey $FABRIC_APIKEY" ./ios/example/Info.plist
